0e0fff706ea1f3c75b9f0f9870e991e564f23945
akatski316 <battleofsunny@gmail.com>
2020-10-18 04:18:02 -0700

    My dashboard is ready

--- src/Pages/Mydashboard/Tabs/QuickConcepts_Tab/QuickConceptsTab.js
+++ src/Pages/Mydashboard/Tabs/QuickConcepts_Tab/QuickConceptsTab.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import background from "../../../../assets/image_card_ml.svg";
-import { WideCardContainer, WideCard, WideCardBackground, TextContainer, FeatureText, CardTitle } from "./Styles";
+import img1 from "../../../../assets/image_quickconcepts_ml.png";
+import img2 from "../../../../assets/image_quickconcepts_ml2.png";
+import img3 from "../../../../assets/image_quickconcepts_ml3.png";
+import img4 from "../../../../assets/image_quickconcepts_ml4.png";
+
+import { WideCardContainer, WideCard, TextContainer, FeatureText, LargeCardTitle, BottomCardContainer } from "./Styles";
+import {HorizontalCardContainer, Card, SmallCardTitle, TimeToRead, LightText} from "./Styles";
+
 
 class QuickConceptsTab extends React.Component {
      render() {
@@ -9,21 +16,56 @@ class QuickConceptsTab extends React.Component {
                <div>
                     <WideCardContainer>
                          <WideCard margin="0%" src={background} >
-                              {/* <WideCardBackground src={background} /> */}
-                              {/* <TextContainer>
+                              
+                               <TextContainer>
                                    <FeatureText>Quick Learn</FeatureText>
-                                   <CardTitle>shvbksbvkjsbkjsbdvkj</CardTitle>
-                              </TextContainer> */}
+                                   <LargeCardTitle>Machine Learning vs Deep Learning</LargeCardTitle>
+                                   <BottomCardContainer><FeatureText>10 min course</FeatureText></BottomCardContainer>
+                              </TextContainer>
                          </WideCard>
 
-                         <WideCard margin="5%" src={background}>
-                              {/* <WideCardBackground src={background} /> */}
-                              {/* <TextContainer>
+                         <WideCard margin="3%" src={background}>
+                              
+                               <TextContainer>
                                    <FeatureText>Quick Learn</FeatureText> 
-                                   <CardTitle>shvbksbvkjsbkjsbdvkj</CardTitle>
-                              </TextContainer> */}
+                                   <LargeCardTitle>Machine Learning vs Deep Learning</LargeCardTitle>
+                                   <BottomCardContainer><FeatureText>5 min Course</FeatureText></BottomCardContainer>
+                              </TextContainer>
                          </WideCard>
                     </WideCardContainer>
+
+                    <HorizontalCardContainer>
+                         <Card margin = "0%">
+                              <img src = {img1}/>
+                              <SmallCardTitle>How business needs to change to be AI ready?</SmallCardTitle>
+                              <TimeToRead>6 min read</TimeToRead>
+                              <LightText>Business Insider</LightText>
+
+                         </Card >
+
+                         <Card margin = "2%">
+                              <img src = {img2}/>
+                              <SmallCardTitle>Is neuralink actually gonna save world?</SmallCardTitle>
+                              <TimeToRead>10 min read</TimeToRead>
+                              <LightText>Medium</LightText>
+
+                         </Card>
+
+                         <Card margin = "2%">
+                              <img src = {img3}/>
+                              <SmallCardTitle>A primer on Artificial Intelligence (AI)</SmallCardTitle>
+                              <TimeToRead>5 min read</TimeToRead>
+                              <LightText>Hackernoon</LightText>
+
+                         </Card>
+
+                         <Card margin = "2%">
+                              <img src = {img4}/>
+                              <SmallCardTitle>Machine learning vs Deep learning</SmallCardTitle>
+                              <TimeToRead>8 min read</TimeToRead>
+                              <LightText>Medium</LightText>
+                         </Card>
+                    </HorizontalCardContainer>
                </div>
           );
 
--- src/Pages/Mydashboard/Tabs/QuickConcepts_Tab/Styles.js
+++ src/Pages/Mydashboard/Tabs/QuickConcepts_Tab/Styles.js
@@ -1,51 +1,74 @@
 import styled from 'styled-components';
 
 
-
 export const WideCardContainer = styled.div`
      display: flex;
 `;
 
 export const WideCard = styled.div`
      background-color: aliceblue;
-     width: 45%;
+     width: 50%;
      height: 250px;
      margin-left: ${props => props.margin};
      background-image: url(${props => props.src});
      background-repeat: no-repeat;
      background-position: left 4em top;
      background-size: 100% 100%;
-
-`;
-
-export const WideCardBackground = styled.img`
-     z-index: -5;
-     width: 80%;
-     height: 80%;
-     float: right;
-     background-color: red;
+     border-radius: 20px;
+     overflow: hidden;
 `;
 
 export const FeatureText = styled.div`
         color: grey;
-        font-family: Helvetica;
-        z-index: 2;         
+        font-family: Helvetica;      
+        margin-top: 10%;  
 `;
 
-export const CardTitle = styled.div`
-        font-size: 40px;
-        font-weight: 300;
-        margin-bottom: 10%;
+export const LargeCardTitle = styled.div`
+        font-size: 22px;
+        font-weight: bold;
+        margin-bottom: 12%;
+        margin-top: 3%;
+        width: 60%;
 `;
 
 export const TextContainer = styled.div`
      width: 100%;
      height: 100%;
-     padding-left:10%; 
-     z-index: 2;  
+     padding-left:10%;      
      float: left;
-     /* background-color: green;        */
+`;
 
+export const BottomCardContainer = styled.div`
+     width: 100%;
+`;
+
+export const HorizontalCardContainer = styled.div`
+     width: 100%;
+     margin-top: 3%;
+     display: flex;
+`;
+
+export const Card = styled.span`
+     margin-left: ${props => props.margin};
+     width:25%;
 `;
 
 
+export const SmallCardTitle = styled.div`
+     font-size: 17px;
+     font-weight: bold;
+     margin-bottom: 7%;
+     margin-top: 7%;
+     width: 80%;  
+`;
+
+export const TimeToRead = styled.span`
+     font-size: 12px;
+`;
+
+export const LightText = styled.span`
+     font-size: 12px;
+     opacity: 0.5;
+     margin-left: 1%;
+`;
