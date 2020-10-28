6f4deada4e24726e990003f5eb87f23a33b98dd0
akatski316 <battleofsunny@gmail.com>
2020-10-28 12:47:16 -0700

    password validation added

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -26,14 +26,27 @@ export default class Login extends React.Component {
 
      signUp = (e) => {
           e.preventDefault();
-          firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-               .then((user) => {
-                    console.log(user);
-               }).catch((error) => {
-                    console.log(error);
-                    alert("Something went wrong");
+          if(this.ValidateEmail(this.state.email)){
+               firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+                    .then((user) => {
+                         console.log(user);
+                    }).catch((error) => {
+                         console.log(error);
+                         alert(error.message);
 
-               })
+                    })
+          }
+     }
+
+     ValidateEmail(inputText) {
+          var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+          if (inputText.match(mailformat)) {
+               return true;
+          }
+          else {
+               alert("You have entered an invalid email address!");    //The pop up alert for an invalid email address
+               return false;
+          }
      }
 
      handleChange = (e) => {
